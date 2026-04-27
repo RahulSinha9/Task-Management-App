e2a5d8daf66088b13b33808da4ba015b26e8035c
root <root@ip-172-31-34-49.ap-south-1.compute.internal>
2026-04-27 16:18:35 +0000

    fix: rebuild production with correct API URL

---
+++ infra/outputs.tf
@@ -0,0 +1,43 @@
+output "ecr_backend_url" {
+  value = aws_ecr_repository.backend.repository_url
+}
+
+output "ecr_frontend_url" {
+  value = aws_ecr_repository.frontend.repository_url
+}
+
+output "vpc_id" {
+  value = aws_vpc.arm-task.id
+}
+
+output "public_subnet_a" {
+  value = aws_subnet.public_a.id
+}
+
+output "private_subnet_a" {
+  value = aws_subnet.private_a.id
+}
+
+output "secrets_manager_arn" {
+  value = aws_secretsmanager_secret.arm-task.arn
+}
+
+output "sns_topic_arn" {
+  value = aws_sns_topic.alerts.arn
+}
+
+output "ec2_instance_id" {
+  value = aws_instance.app.id
+}
+
+output "bastion_public_ip" {
+  value = aws_instance.bastion.public_ip
+}
+
+output "staging_instance_id" {
+  value = aws_instance.staging.id
+}
+
+output "staging_private_ip" {
+  value = aws_instance.staging.private_ip
+}
---
+++ infra/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "aws_account_id" {
+  default = "773802564338"
+}
+
+variable "admin_ip_cidr" {
+  default = "15.206.158.67/32"
+}
+
+variable "ec2_instance_type" {
+  default = "t3.small"
+}
+
+variable "alert_email" {
+  default = "krahul088176@gmail.com"   # change this
+}
